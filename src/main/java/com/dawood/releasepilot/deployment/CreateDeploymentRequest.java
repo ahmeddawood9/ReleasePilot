f1a1c3331321c732278928b9b45c5a60ae377cb1
Dawood <dawoodsoomro56@gmail.com>
2026-06-06 15:57:39 +0500

    feat: add deployment create request DTO

--- src/main/java/com/dawood/releasepilot/deployment/CreateDeploymentRequest.java
+++ src/main/java/com/dawood/releasepilot/deployment/CreateDeploymentRequest.java
@@ -1,1 +1,7 @@
 package com.dawood.releasepilot.deployment;
+
+//record = simple immutable data carrier
+//this represents input data coming from our backend.
+
+
+public record CreateDeploymentRequest (String serviceName,String version) {}
