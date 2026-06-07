5a88e76695618425d82e7b8e4a49cce5f4a1914f
Dawood <dawoodsoomro56@gmail.com>
2026-06-07 12:25:11 +0500

    feat: add deployment service repository dependency

--- src/main/java/com/dawood/releasepilot/deployment/DeploymentService.java
+++ src/main/java/com/dawood/releasepilot/deployment/DeploymentService.java
@@ -1,1 +1,17 @@
 package com.dawood.releasepilot.deployment;
+
+// Service layer = main backend logic layer.
+// It coordinates use cases.
+//
+// Service uses:
+// - DTOs
+// - domain objects
+// - repository
+// - exceptions
+public class DeploymentService {
+    private final DeploymentRepository deploymentRepository;
+
+    public DeploymentService(DeploymentRepository deploymentRepository) {
+        this.deploymentRepository = deploymentRepository;
+    }
+}
