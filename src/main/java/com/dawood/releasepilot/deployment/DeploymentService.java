aff10742b84ac97fb4c253fa1588b1ee908c2d03
Dawood <dawoodsoomro56@gmail.com>
2026-06-07 12:25:34 +0500

    feat: create deployments through service

--- src/main/java/com/dawood/releasepilot/deployment/DeploymentService.java
+++ src/main/java/com/dawood/releasepilot/deployment/DeploymentService.java
@@ -14,4 +14,37 @@ public class DeploymentService {
     public DeploymentService(DeploymentRepository deploymentRepository) {
         this.deploymentRepository = deploymentRepository;
     }
+
+    // Use case 1:
+    // Create a new deployment.
+    public DeploymentResponse createDeployment(CreateDeploymentRequest request) {
+        // Validate request object itself.
+        if (request == null) {
+            throw new IllegalArgumentException("CreateDeploymentRequest cannot be null");
+        }
+
+        // Create domain object from request DTO.
+        Deployment deployment = new Deployment(
+                null,
+                request.serviceName(),
+                request.version()
+        );
+
+        // Save using repository.
+        Deployment savedDeployment = deploymentRepository.save(deployment);
+
+        // Convert internal domain object into response DTO.
+        return toResponse(savedDeployment);
+    }
+
+    // Private mapper method.
+    // Converts internal Deployment object into DeploymentResponse DTO.
+    private DeploymentResponse toResponse(Deployment deployment) {
+        return new DeploymentResponse(
+                deployment.getId(),
+                deployment.getServiceName(),
+                deployment.getVersion(),
+                deployment.getStatus()
+        );
+    }
 }
