64510f0030abb747b8ad828c07f56b79fa3c4ad8
Dawood <dawoodsoomro56@gmail.com>
2026-06-07 12:31:47 +0500

    feat: wire manual deployment service runner

--- src/main/java/com/dawood/releasepilot/Main.java
+++ src/main/java/com/dawood/releasepilot/Main.java
@@ -1,1 +1,18 @@
 package com.dawood.releasepilot;
+
+import com.dawood.releasepilot.deployment.DeploymentRepository;
+import com.dawood.releasepilot.deployment.DeploymentService;
+import com.dawood.releasepilot.deployment.InMemoryDeploymentRepository;
+
+// Main is only for manual testing right now.
+// Later, Spring Boot Controller will replace this manual flow.
+public class Main {
+    public static void main(String[] args) {
+        // Create repository.
+        // This is our fake database.
+        DeploymentRepository repository = new InMemoryDeploymentRepository();
+
+        // Create service and inject repository into it.
+        DeploymentService service = new DeploymentService(repository);
+    }
+}
